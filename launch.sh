b3cf2c0c1e3521d99fd3ac3d3c9d2e0a2f2de122
Cristian Bassotto <cristianbassotto21@gmail.com>
2023-06-01 12:53:19 +0200

    creato file di setup dell'applicazione

--- launch.sh
+++ launch.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-
-if [ -f ./install/lib/gui_pkg/start_gui_node ]
-	then
-		echo file trovato
-		./install/lib/gui_pkg/start_gui_node
-	else
-		echo file non trovato
-fi
+./install/lib/gui_pkg/start_gui_node
---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Installa ROS Noetic (required)
+sudo apt install -y ros-noetic-desktop-full
+
+# Inizializza l'ambiente di sviluppo di ROS Noetic
+source /opt/ros/noetic/setup.bash
+
+# Esegui catkin_make install
+catkin_make install
+
+# Controlla se il file ./install/lib/gui_pkg/start_gui_node esiste
+if [ -f ./install/lib/gui_pkg/start_gui_node ]; then
+    echo "Executable created. Now you can launch the launch.sh"
+    echo "#!/bin/bash" > launch.sh
+    echo "./install/lib/gui_pkg/start_gui_node" >> launch.sh
+    
+    # Rendi il file launch.sh eseguibile
+    chmod +x launch.sh
+else
+    echo "Executable not created, retry."
+fi
